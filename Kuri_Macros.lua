bcc6144acd825f3cd14db583bd37bc71d834f236
Guillaume Friloux <guillaume@friloux.me>
2020-12-12 18:26:38 +0100

    Fix macro icons

--- Kuri_Macros.lua
+++ Kuri_Macros.lua
@@ -1,8 +1,8 @@
 -- Find icon names at https://classic.wowhead.com/icons?filter=2;2;0
 
-Zorlen_MakeMacro("KDPS-Duals", "/run kuri_fury_dual_strike()", 0, "Spell_Holy_Blessingofstrength", nil, 1, show)
-Zorlen_MakeMacro("KDPS-AoE", "/run kuri_fury_aoe()", 0, "Spell_Fire_Selfdestruct", nil, 1, show)
-Zorlen_MakeMacro("KTank", "/run kuri_tank()", 0, "Ability_Warrior_Innerrage", nil, 1, show)
+Zorlen_MakeMacro("KDPS-Duals", "/run kuri_fury_dual_strike()", 0, "Ability_Warrior_Challange", nil, 1, show)
+Zorlen_MakeMacro("KDPS-AoE", "/run kuri_fury_aoe()", 0, "Spell_Fire_SelfDestruct", nil, 1, show)
+Zorlen_MakeMacro("KTank", "/run kuri_tank()", 0, "Ability_Warrior_InnerRage", nil, 1, show)
 Zorlen_MakeMacro("KOff-Tank", "/run kuri_tank_off()", 0, "Spell_Nature_Reincarnation", nil, 1, show)
 Zorlen_MakeMacro("KSetAssist", "/run Zorlen_changeAssist()", 0, "Spell_Holy_DivineSpirit", nil, 1, show)
-Zorlen_MakeMacro("KAssist", "/run Zorlen_assist()", 0, "Spell_Holy_Prayerofspirit", nil, 1, show)
+Zorlen_MakeMacro("KAssist", "/run Zorlen_assist()", 0, "Spell_Holy_PrayerofSpirit", nil, 1, show)
