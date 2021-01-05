13e282cb602e831b956797a5cba7190a2af01b56
Guillaume Friloux <guillaume@friloux.me>
2021-01-05 11:56:15 +0100

    Really disable macro creation

--- Kuri_Macros.lua
+++ Kuri_Macros.lua
@@ -1,9 +1,11 @@
 -- Find icon names at https://classic.wowhead.com/icons?filter=2;2;0
 
-Zorlen_MakeMacro("KDPS-Duals", "/run kuri_fury_dual_strike()", 0, "Ability_Warrior_Challange", nil, 1)
-Zorlen_MakeMacro("KDPS-AoE", "/run kuri_fury_aoe()", 0, "Spell_Fire_SelfDestruct", nil, 1)
-Zorlen_MakeMacro("KTank", "/run kuri_tank()", 0, "Ability_Warrior_InnerRage", nil, 1)
-Zorlen_MakeMacro("KOff-Tank", "/run kuri_tank_off()", 0, "Spell_Nature_Reincarnation", nil, 1)
-Zorlen_MakeMacro("KSetAssist", "/run Zorlen_changeAssist()", 0, "Spell_Holy_DivineSpirit", nil, 1)
-Zorlen_MakeMacro("KAssist", "/run Zorlen_assist()", 0, "Spell_Holy_PrayerofSpirit", nil, 1)
-Zorlen_MakeMacro("KClearAssist", "/run Zorlen_clearAssist()", 0, "Ability_BullRush", nil, 1)
+function kuri_tank() 
+	Zorlen_MakeMacro("KDPS-Duals", "/run kuri_fury_dual_strike()", 0, "Ability_Warrior_Challange", nil, 1)
+	Zorlen_MakeMacro("KDPS-AoE", "/run kuri_fury_aoe()", 0, "Spell_Fire_SelfDestruct", nil, 1)
+	Zorlen_MakeMacro("KTank", "/run kuri_tank()", 0, "Ability_Warrior_InnerRage", nil, 1)
+	Zorlen_MakeMacro("KOff-Tank", "/run kuri_tank_off()", 0, "Spell_Nature_Reincarnation", nil, 1)
+	Zorlen_MakeMacro("KSetAssist", "/run Zorlen_changeAssist()", 0, "Spell_Holy_DivineSpirit", nil, 1)
+	Zorlen_MakeMacro("KAssist", "/run Zorlen_assist()", 0, "Spell_Holy_PrayerofSpirit", nil, 1)
+	Zorlen_MakeMacro("KClearAssist", "/run Zorlen_clearAssist()", 0, "Ability_BullRush", nil, 1)
+end
