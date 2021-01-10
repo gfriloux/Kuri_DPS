5dd549f104ee42491dcf8a9877ddd0fafc35ce12
Guillaume Friloux <guillaume@friloux.me>
2021-01-10 17:05:25 +0100

    Avoid spamming debuffs on immune mobs

--- Kuri_Debuff.lua
+++ Kuri_Debuff.lua
@@ -1,14 +1,25 @@
+local DemoTime    = 0;
+local ThunderTime = 0;
+
 function kuri_debuff_attack()
+	local now = GetTime();
+
 	if UnitInRaid("player") then
 		-- We debuff Power Attack if target is at distance
 		-- And not already affected
 		if not isDemoralized("target") then
-			castDemoralizingShout()
+			if now - DemoTime > 20 then
+				DemoTime = now
+				castDemoralizingShout()
+			end
 		end
 
 		-- We debuff Attack Speed
 		if not isThunderClap("target") then
-			castThunderClap()
+			if now - ThunderTime > 20 then
+				ThunderTime = now
+				castThunderClap()
+			end
 		end
 
 		-- We want max sunder armor for maximum DPS ASAP
